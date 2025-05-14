a586da6553296117f27098d07205bb1c1fb3bbc8
Rin <794922335@qq.com>
2025-05-14 17:02:10 +0800

    fix bugs

--- src/utils.py
+++ src/utils.py
@@ -167,7 +167,8 @@ class Utils:
             self.get_weather_map()
         except FileDownloadError:
             raise
-    
+        
+    @staticmethod
     def classify_day(timestamp: int) -> str:
         dt = datetime.fromtimestamp(timestamp)
         date = dt.date()
